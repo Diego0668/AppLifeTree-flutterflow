c141675c42380a20458efac79470255376c5fe60
Diego0668 <diegolimaxd1@gmail.com>
2023-09-01 22:00:14 -0300

    update

--- android/app/src/main/java/io/flutter/plugins/ClienteDao.java
+++
@@ -1,7 +0,0 @@
-package android.app.src.main.java.io.flutter.plugins;
-
-public class ClienteDao {
-    public static final String TAG ="CRUD Cliente";
-
-    public static int
-}
--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mycompany.lifetreemobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
