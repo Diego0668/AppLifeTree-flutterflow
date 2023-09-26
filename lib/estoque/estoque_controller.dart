f1d66a5a6a4c6e044586c7d71f1cb60aaa7618fc
Diego Augusto <80727709+Diego0668@users.noreply.github.com>
2023-09-25 21:24:05 -0300

    Update estoque_controller.dart

--- lib/estoque/estoque_controller.dart
+++ lib/estoque/estoque_controller.dart
@@ -3,30 +3,28 @@ import 'dart:ffi';
 import 'package:dio/dio.dart';
 import '../models/produto_model.dart';
 
-
-class ProdutoController{
+class EstoqueController{
 
   ProdutoModel model = ProdutoModel();
   Dio dio = Dio();
 
-  Future<bool> MostrarProduto({required Int id , required String nome , required ByteConversionSink imagem , required String statusProd , required String quantidade}) async{
-    dynamic dados2 = jsonEncode(<Int, String, ByteConversionSink, String, String>){
-      
-      
-      "id": id.toString();
-      "nome": nome.toString();
-      "imagem":imagem.toString();
-      "statusProd":statusProd.toString();
-      "quantidade":quantidade.toString();
+  Future<bool> listarProdutos({required Int id, required String nome, required String imagem , required String statusProd , required String quantidade}) async {
 
-      try {
+    dynamic dados = jsonEncode(<String, String>{
+                    "id": id.toString(),
+                    "nome": nome.toString(),
+                    "imagem": imagem.toString(),
+                    "statusProd": statusProd.toString(),
+                    "quantidade": quantidade.toString()
+                });
+    try {
         dynamic response = await dio.post('http://localhost:8080/api/lifetree/funcionario/produtosApi', 
               options: Options(contentType: 'application/json; charset=UTF-8', headers: {'Access-Control-Allow-Origin':'*'}), 
-              data: dados2);
+              data: dados);
         if(response.statusCode == 200){
           //print(response.data);
           var resultado = Map.from(response.data);
-          model.produto = resultado.values.elementAt(2);
+          model.nome = resultado.values.elementAt(2);
           //print(model.usuario);
           return true;
         }
@@ -34,15 +32,7 @@ class ProdutoController{
       print(e);
     }
     return false;
-
-
-    };
-
-  
-  
   }
+  
+}   
 
-
-
-
-}
